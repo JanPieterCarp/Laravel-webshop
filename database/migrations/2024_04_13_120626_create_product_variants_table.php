96c291f925a031df480c3456a8777cb9da9ae853
JanPieterCarp <jpott2000@gmail.com>
2024-04-13 14:24:59 +0200

    project set-up + migration and model scafolding

---
+++ database/migrations/2024_04_13_120626_create_product_variants_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_variants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger(column: 'product_id');
+            $table->unsignedInteger(column: 'color');
+            $table->unsignedInteger(column: 'size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_variants');
+    }
+};
